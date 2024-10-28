4382e8d196ba7b43bb0858a2895ca74820f3d011
lucascantao <cantao162@gmail.com>
2024-10-28 00:54:37 -0300

    interface da página inicial

--- resources/views/pages/repo/index.blade.php
+++ resources/views/pages/repo/index.blade.php
@@ -3,10 +3,11 @@
 
 <div class="sidebar">
     <div class="sidebarHeader">
-        ...
+        <span><img src="/images/icons/riot-games.png" alt="" height="48px"></span>
+        <span><i class="bi bi-chevron-bar-left"></i></span>
     </div>
 
-    <span>Sobre</span>
+    <span class="btn btn-secondary sobre"><i class="bi bi-info-circle"></i> Sobre</span>
 
     <div class="sidebarMenu">
         <div class="menuTitle">Projeto Recentes</div>
